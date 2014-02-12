ee2a08d451c599734463d82ad5160de0181f83e3
Tyler Schade <tylerschade99@gmail.com>
2014-02-12 09:11:37 -0500

    Tiny theme
    Initial commit

---
+++ front-page.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+
+<div class="body">
+			<section id="intro">
+				<h1>Welcome!</h1>
+				Hi! My name is Tyler Schade. I am a freelance mobile and web developer. I write code, build apps, and make websites. I work with talented designers to build <strong>your</strong> product. I have worked on a bunch of projects, utilizing multiple different skillsets. <a href="#about">Learn more about me.</a>
+			</section>
+			<section id="about">
+				<h1>About Me</h1>
+				I began learning web development in the middle of 2012. I was interested in learning something that I could use to earn and that would challenge me. I learned the basics of HTML and CSS, and then took a break for a while. Then in the summer of 2013, I picked it up again and learned Javascript. I started working on projects for other people that summer, starting with Wordpress. I worked on a few Wordpress sites, and then worked on an HTML5 mobile app. Next I built the <a href="http://bridgeortunnel.com">bridgeOrTunnel</a> app and website. I continue to look for projects that will challenge me to expand my skills and learn new things. Think you have a project for me? <a href="#contact">Let me know!</a>
+			</section>
+			<section id="skills">
+				<h1>Skills</h1>
+				I have skills in the following areas:
+				<ul>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>Javascript</li>
+					<li>PHP</li>
+					<li>MySQL</li>
+					<li>SASS</li>
+					<li>Python</li>
+					<li>Django</li>
+					<li>Responsive web development</li>
+					<li>Phonegap</li>
+				</ul>
+				<p>I am always open to learning new things, pushing the limits of what I know. I have never taken on a project that I knew everything needed to complete it sucessfully beforehand.</p>
+			</section>
+			<section id="made">
+				<h1>Things I Made</h1>
+				<p>More coming soon!</p>
+			</section>
+			<section id="contact">
+				<h1>Contact Me</h1>
+				<form>
+					<label>Name:</label><br>
+					<input type="text" required name="name"><br>
+					<label>Email Address:</label><br>
+					<input type="email" required name="email"><br>
+					<label>Message:</label><br>
+					<textarea>
+						
+					</textarea>
+				</form>
+			</section>
+		</div>
+<?php get_footer(); ?>
---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+		<title>
+			Tyler Schade | Freelance Web Developer
+		</title>
+	</head>
+	<body>
+		<header>
+			<span class="logo"><a href="http://tjsdesigned.com">tjsdesigned</a></span>
+			<p>&quot;i make things with code&quot;</p>
+			<nav><ul><li><a href="#">home</a></li><li><a href="#about">about</a></li><li><a href="#skills">skills</li><li><a href="#made">things i made</a></li><li><a href="#contact">contact</a></li></ul></nav>
+		</header>
+		
